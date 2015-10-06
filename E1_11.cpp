6ae9b16cab39f4fda43cb4be6264815783e1dfce
Sean Lao <slao258@gmail.com>
2015-10-05 19:57:35 -0700

    finish E1.9, E1.10, E1.11

---
+++ E1_11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+	int val1 = 0, val2 = 0;
+	std::cout << "Enter two numbers" << std::endl;
+	std::cin >> val1 >> val2;
+
+	while (val1 <= val2)
+	{
+		std::cout << val1 << std::endl;
+		++val1;
+	}
+	return 0;
+}
