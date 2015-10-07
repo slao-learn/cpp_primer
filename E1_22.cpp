f140a2a68547236afafc301c51dc8f2a46898700
Sean Lao <slao258@gmail.com>
2015-10-07 15:52:18 -0700

    finish 1.22

---
+++ E1_22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+	Sales_item sum, item;
+
+	while (std::cin >> item)
+	{
+		sum += item;
+	}
+	std::cout << sum << std::endl; // print their sum
+	return 0;
+}
+
